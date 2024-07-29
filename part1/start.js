62af8b919084fa825f6faa3a87dfad389ce73965
abi-hong <flffjakfwm20@gmail.com>
2024-07-29 09:17:42 +0900

    chore: change year data

--- part1/start.js
+++ part1/start.js
@@ -24,18 +24,18 @@ const svg = d3.select("#chart-container")
 // Create a fake dataset
 
 const dataset = [
-  { date: new Date("2022-01-01"), value: 200 },
-  { date: new Date("2022-02-01"), value: 250 },
-  { date: new Date("2022-03-01"), value: 180 },
-  { date: new Date("2022-04-01"), value: 300 },
-  { date: new Date("2022-05-01"), value: 280 },
-  { date: new Date("2022-06-01"), value: 220 },
-  { date: new Date("2022-07-01"), value: 300 },
-  { date: new Date("2022-08-01"), value: 450 },
-  { date: new Date("2022-09-01"), value: 280 },
-  { date: new Date("2022-10-01"), value: 600 },
-  { date: new Date("2022-11-01"), value: 780 },
-  { date: new Date("2022-12-01"), value: 320 }
+  { date: new Date("2024-01-01"), value: 200 },
+  { date: new Date("2024-02-01"), value: 250 },
+  { date: new Date("2024-03-01"), value: 180 },
+  { date: new Date("2024-04-01"), value: 300 },
+  { date: new Date("2024-05-01"), value: 280 },
+  { date: new Date("2024-06-01"), value: 220 },
+  { date: new Date("2024-07-01"), value: 300 },
+  { date: new Date("2024-08-01"), value: 450 },
+  { date: new Date("2024-09-01"), value: 280 },
+  { date: new Date("2024-10-01"), value: 600 },
+  { date: new Date("2024-11-01"), value: 780 },
+  { date: new Date("2024-12-01"), value: 320 }
 ];
 
 // Define the x and y domains
